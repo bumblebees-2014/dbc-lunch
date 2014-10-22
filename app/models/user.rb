d4b8a3c9cd2a0068f76bbb4994e26f0a7abc059b
Alex Ung <alexung.ucsb@gmail.com>
2014-10-22 13:52:01 -0400

    Has_secure_password

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,10 +3,4 @@ class User < ActiveRecord::Base
 
   has_secure_password
 
-  def self.authenticate(username, password)
-      user = User.find_by(username: username)
-      return user.authenticate(password) unless user == nil
-      nil
-  end
-
 end
