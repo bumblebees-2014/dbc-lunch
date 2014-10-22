a2ac7d9ad32ecee0fa5454705a949c22e664b74b
Alex Ung <alexung.ucsb@gmail.com>
2014-10-22 12:05:35 -0400

    Add the correct gems

--- app/controllers/user.rb
+++ app/controllers/user.rb
@@ -1,9 +1,41 @@
+enable :sessions
+
 get '/session' do
   erb :'users/login'
 end
 
+post '/session' do
+  user = User.find_by(username: params[:username])
+
+  if user.nil?
+    session[:error] = "Login failed"
+    redirect "/session"
+  end
+
+  user = user.authenticate(params[:password])
+
+  if user
+    session[:user_id] = user.id
+    redirect "/ride"
+  else
+    session[:error] = "Login failed"
+    redirect '/session'
+  end
+
+end
+
 get '/user' do
   erb :'users/create_user'
+end
+
+post '/user' do
+  user = User.new(params[:user])
+  if user.valid?
+    user.save
+    redirect '/session'
+  else
+    redirect '/user'
+  end
 end
 
 get '/logout' do
--- config/environment.rb
+++ config/environment.rb
@@ -8,7 +8,7 @@ require 'bundler/setup' if File.exists?(ENV['BUNDLE_GEMFILE'])
 # Require gems we care about
 require 'rubygems'
 require 'pry'
-require 'bcrypt'
+gem 'bcrypt-ruby', '~> 3.0.0'
 
 require 'uri'
 require 'pathname'
